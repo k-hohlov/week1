44440550b3671c1badf211228f12ba74efd19eb5
Konstantin Khokhlov <ka.hohlov@yandex.ru>
2021-11-19 12:43:17 +0300

    Учтены замечания наставника

--- HW1/ex5.php
+++ HW1/ex5.php
@@ -1,40 +1,36 @@
 <?php
 
-$bmw = array(
+$bmw = [
     "model" => "X5",
     "speed" => 120,
     "doors" => 5,
-    "year" => "2015"
-);
+    "year" => 2015
+];
 
-$toyota = array(
+$toyota = [
     "model" => "Camry",
     "speed" => 150,
     "door" => 4,
-    "year" => "2014"
-);
+    "year" => 2014
+];
 
-$opel = array(
+$opel = [
     "model" => "Astra",
     "speed" => 140,
     "door" => 3,
-    "year" => "2013"
-);
+    "year" => 2013
+];
 
-$cars = array(
+$cars = [
     "bmw" => $bmw,
     "toyota" => $toyota,
     "opel" => $opel
-);
+];
 
 #echo '<pre>';
 #var_dump($cars);
 
 foreach ($cars as $key => $value) {
-    echo "CAR $key<br>";
-    echo implode(' ', $value);
-    //foreach ($value as $item) {
-    //    print("$item "); //TODO
-    //}
+    echo "CAR $key<br>", implode(' ', $value);
     echo "<br>";
 }
